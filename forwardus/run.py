26bb4d434a1417a98c5b2b0be4467d3fc112f9c7
hys7628 <hys7628@gmail.com>
2026-09-22 14:38:12 +0900

    0922_yoonsu_master_02

--- forwardus/run.py
+++ forwardus/run.py
@@ -19,3 +19,5 @@ if __name__ == "__main__":
     # 템플릿·파이썬 코드를 바꿔도 서버를 껐다 켜기 전까지 화면이 그대로입니다.
     app.run(host="0.0.0.0", port=app.config["PORT"],
             debug=app.config["DEBUG"], use_reloader=True)
+
+
